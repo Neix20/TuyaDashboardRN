d144343f8fca09ecad1095fbd8797c807dee9131
Neix20 <txen2000@gmail.com>
2023-08-14 15:32:39 +0800

    Fixed iOS

--- android/app/src/main/java/com/yatudashboard/MainActivity.java
+++ android/app/src/main/java/com/yatudashboard/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Yatu Dashboard";
+    return "YatuDashboard";
   }
 
   @Override
