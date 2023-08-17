c58f21fb7ada35c5281a8a66e1d19e51d94a717f
Neix20 <txen2000@gmail.com>
2023-08-17 12:07:36 +0800

    Temporarily Removed rnTuyaSDK

--- app/screens/Debug/index.js
+++ app/screens/Debug/index.js
@@ -18,6 +18,8 @@ import { Checkbox as PaperCheckbox } from "react-native-paper";
 import { CheckBox as ElemCheckbox } from '@rneui/base';
 import { Checkbox as NativeCheckbox } from "native-base";
 
+import WChart from "./WChart";
+
 // #region Trash
 function Chart(props) {
     const toast = useToast();
@@ -217,73 +219,15 @@ function DebugRange(props) {
         </>
     )
 }
-
-// Choose your preferred renderer
-// import { SkiaChart, SVGRenderer } from '@wuba/react-native-echarts';
-import { SvgChart, SVGRenderer } from '@wuba/react-native-echarts';
-import * as echarts from 'echarts/core';
-import { BarChart } from 'echarts/charts';
-import { TitleComponent, TooltipComponent, GridComponent } from 'echarts/components';
-
-// Register extensions
-echarts.use([
-    TitleComponent,
-    TooltipComponent,
-    GridComponent,
-    SVGRenderer,
-    BarChart,
-])
-
-const E_HEIGHT = 250;
-const E_WIDTH = 300;
-
-// Initialize
-function ChartComponent(props) {
-
-    const { option } = props;
-    const chartRef = useRef(null);
-
-    useEffect(() => {
-        let chart;
-        if (chartRef.current) {
-            // @ts-ignore
-            chart = echarts.init(chartRef.current, 'light', {
-                renderer: 'svg',
-                width: E_WIDTH,
-                height: E_HEIGHT,
-            });
-            chart.setOption(option);
-        }
-        return () => chart?.dispose();
-    }, [option]);
-
-    // Choose your preferred chart component
-    // return <SkiaChart ref={chartRef} />;
-    return (
-        <SvgChart ref={chartRef} />
-    );
-}
-
-// Component usage
+ 
 function Index() {
-    const option = {
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        },
-        yAxis: {
-            type: 'value',
-        },
-        series: [
-            {
-                data: [120, 200, 150, 80, 70, 110, 130],
-                type: 'bar',
-            },
-        ],
-    }
 
     return (
-        <ChartComponent option={option} />
+        <SafeAreaView style={{flex: 1}}>
+            <View flex={1} alignItems={"center"} justifyContent={"center"}>
+                <WChart />
+            </View>
+        </SafeAreaView>
     )
 }
 
