1372cb810554e54642d7a58c59a5946e04fb5fc1
Neix20 <txen2000@gmail.com>
2023-08-17 11:46:19 +0800

    Added Skia & Update iOS Tuya

--- app/screens/Debug/index.js
+++ app/screens/Debug/index.js
@@ -137,7 +137,7 @@ function TestCheckbox(props) {
 }
 // #endregion
 
-function Index(props) {
+function DebugRange(props) {
 
     // #region Init
     const init = {
@@ -165,7 +165,7 @@ function Index(props) {
                 <View style={{ flex: 1 }}>
 
                     <VStack space={3} p={3}>
-                        
+
                         <Text style={{
                             fontFamily: "Roboto-Bold",
                             fontSize: 16
@@ -215,6 +215,75 @@ function Index(props) {
                 </View>
             </SafeAreaView>
         </>
+    )
+}
+
+// Choose your preferred renderer
+// import { SkiaChart, SVGRenderer } from '@wuba/react-native-echarts';
+import { SvgChart, SVGRenderer } from '@wuba/react-native-echarts';
+import * as echarts from 'echarts/core';
+import { BarChart } from 'echarts/charts';
+import { TitleComponent, TooltipComponent, GridComponent } from 'echarts/components';
+
+// Register extensions
+echarts.use([
+    TitleComponent,
+    TooltipComponent,
+    GridComponent,
+    SVGRenderer,
+    BarChart,
+])
+
+const E_HEIGHT = 250;
+const E_WIDTH = 300;
+
+// Initialize
+function ChartComponent(props) {
+
+    const { option } = props;
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        let chart;
+        if (chartRef.current) {
+            // @ts-ignore
+            chart = echarts.init(chartRef.current, 'light', {
+                renderer: 'svg',
+                width: E_WIDTH,
+                height: E_HEIGHT,
+            });
+            chart.setOption(option);
+        }
+        return () => chart?.dispose();
+    }, [option]);
+
+    // Choose your preferred chart component
+    // return <SkiaChart ref={chartRef} />;
+    return (
+        <SvgChart ref={chartRef} />
+    );
+}
+
+// Component usage
+function Index() {
+    const option = {
+        xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+            type: 'value',
+        },
+        series: [
+            {
+                data: [120, 200, 150, 80, 70, 110, 130],
+                type: 'bar',
+            },
+        ],
+    }
+
+    return (
+        <ChartComponent option={option} />
     )
 }
 
