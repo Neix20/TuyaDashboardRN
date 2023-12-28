eb7878171b2d8fe8258501c887f2988c583b728a
Neix20 <txen2000@gmail.com>
2023-12-28 14:08:10 +0800

    build: Fixed issue of eCharts not Loading

--- app/screens/Routes/StackNavigation.js
+++ app/screens/Routes/StackNavigation.js
@@ -579,8 +579,8 @@ function Index(props) {
     }
     // #endregion
 
-    // const defaultScreen = (userId == -1 || firstTimeLink) ? "LoginII" : "TabNavigation";
-    const defaultScreen = "Debug";
+    const defaultScreen = (userId == -1 || firstTimeLink) ? "LoginII" : "TabNavigation";
+    // const defaultScreen = "Debug";
 
     return (
         <>
